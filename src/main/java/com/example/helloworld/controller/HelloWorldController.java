b911dd4103ee7ebd1270735d23a478f562c5f417
kocagoz <kocagoz@gmail.com>
2022-06-10 10:18:48 +0300

    ingress, svc rev.

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String sendGreetings() {
         return "Hello, World!";
     }
